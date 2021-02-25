2f2d75a5b415b11272cf8fb9f3312d2f058c0ef4
Felipe Brenner <felipeobrenner@hotmail.com>
2021-02-24 22:03:26 -0300

    Adjusted css

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -39,8 +39,8 @@ export function ChallengeBox() {
             <strong>Finalize um ciclo para receber um desafio</strong>
             <p>
               <img src="icons/level-up.svg" alt="Level Up" />
-            Avance de level completando desafios
-          </p>
+              Avance de level completando desafios
+            </p>
           </div>
         )}
     </div>
